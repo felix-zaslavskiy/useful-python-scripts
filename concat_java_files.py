30fb314cbbfd2df482760a628d14a3a064259c81
felix <felix@zaslavskiy.net>
2023-04-09 11:47:25 -0400

    update

---
+++ concat_java_files.py
@@ -0,0 +1,38 @@
+import sys
+import os
+import re
+
+def process_lines(lines):
+    new_lines = []
+    for line in lines:
+        if not line.startswith('import '):
+            stripped_line = line.rstrip()
+            if stripped_line:
+                new_lines.append(stripped_line + '\n')
+            else:
+                new_lines.append('\n')
+    return new_lines
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python concat_java_files.py <file1.java> <file2.java> ...")
+        sys.exit(1)
+
+    java_files = sys.argv[1:]
+
+    with open("concatenated_output.java", "w") as output_file:
+        for java_file in java_files:
+            with open(java_file, "r") as input_file:
+                # Print "// " before the filename
+                output_file.write(f"// {java_file}\n\n")
+                lines = input_file.readlines()
+                processed_lines = process_lines(lines)
+
+                file_text = "".join(processed_lines)
+                file_text = file_text.rstrip() + '\n'
+
+                output_file.write(file_text)
+
+
+if __name__ == "__main__":
+    main()
---
+++ testpackage/Main.java
@@ -0,0 +1,17 @@
+package testpackage
+
+import Class1;
+import subpackage.Class2;
+
+class Main {
+    static public main(String[] args){
+
+    }
+}
+
+
+
+
+
+
+
