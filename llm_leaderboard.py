afc729472ef7f9db9bf6082ba5a32d996f4f959a
felix <felix@zaslavskiy.net>
2023-05-14 17:59:15 -0400

    update

--- llm_leaderboard.py
+++ llm_leaderboard.py
@@ -1,15 +1,11 @@
+import pickle
+import os
 from gradio_client import Client
 import json
 from bs4 import BeautifulSoup
 
 client = Client("https://huggingfaceh4-open-llm-leaderboard.hf.space/")
-#result = client.view_api(return_format="dict")
-#print(result)
-
-json_data = client.predict(
-    fn_index=2
-)
-
+json_data = client.predict(fn_index=2)
 
 with open(json_data[0], 'r') as file:
     file_data = file.read()
@@ -35,4 +31,24 @@ for d in data_dict:
     # Add the model name and status to the dictionary
     model_status_dict[model_name] = d['Average \u2b06\ufe0f']
 
-print(model_status_dict)
+# Load the previous data if it exists
+if os.path.exists('state.dat'):
+    with open('state.dat', 'rb') as file:
+        previous_data = pickle.load(file)
+else:
+    previous_data = {}
+
+# Save the current data
+with open('state.dat', 'wb') as file:
+    pickle.dump(model_status_dict, file)
+
+# Compare the previous data with the current data
+for key in model_status_dict:
+    if key not in previous_data:
+        print(f'New model: {key}')
+    elif model_status_dict[key] != previous_data[key]:
+        print(f'Model {key} changed from {previous_data[key]} to {model_status_dict[key]}')
+
+for key in previous_data:
+    if key not in model_status_dict:
+        print(f'Model {key} was removed')
