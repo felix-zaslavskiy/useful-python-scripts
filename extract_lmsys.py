7d4dff5649d0555010443e931c7417a8015772c5
felix <felix@zaslavskiy.net>
2023-08-04 11:46:10 -0400

    update

--- extract_lmsys.py
+++ extract_lmsys.py
@@ -4,11 +4,16 @@ import pandas as pd
 url = "https://huggingface.co/spaces/lmsys/chatbot-arena-leaderboard/raw/main/leaderboard_table_20230802.csv"
 df = pd.read_csv(url)
 
-# Filter rows where Link starts with "https://huggingface.co/"
-df = df[df['Link'].str.startswith("https://huggingface.co/")]
+# Filter rows where Link starts with "https://huggingface.co/" or the specific arXiv link
+df = df[df['Link'].str.startswith("https://huggingface.co/") | (df['Link'] == "https://arxiv.org/abs/2302.13971")]
 
-# Extract the Model_ID from the Link column
-df['Model_ID'] = df['Link'].apply(lambda x: x.replace("https://huggingface.co/", ""))
+# Extract the Model_ID from the Link column, with special handling for the arXiv link
+def extract_model_id(link):
+    if link == "https://arxiv.org/abs/2302.13971":
+        return "llama-30b"
+    return link.replace("https://huggingface.co/", "")
+
+df['Model_ID'] = df['Link'].apply(extract_model_id)
 
 # Extract the "MT-bench (score)" column and Model_ID
 result = df[['Model_ID', 'MT-bench (score)']]
