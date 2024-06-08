3e55f5320473fd2e16f9271f0f7137b459f7ce40
felix <felix@zaslavskiy.net>
2024-06-08 07:59:49 -0400

    update

--- concat_java_files.py
+++ concat_java_files.py
@@ -1,6 +1,6 @@
 import sys
 import os
-import re
+import argparse
 
 def process_lines(lines):
     new_lines = []
@@ -12,19 +12,33 @@ def process_lines(lines):
     return new_lines
 
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: python concat_java_files.py <file1.java> <file2.java> ...")
+    parser = argparse.ArgumentParser(description="Concatenate Java files from a list or a directory.")
+    parser.add_argument('-f', '--files', nargs='+', help="List of Java files to concatenate.")
+    parser.add_argument('-d', '--directory', help="Directory containing Java files to concatenate.")
+
+    args = parser.parse_args()
+
+    java_files = []
+    if args.files:
+        java_files = args.files
+    elif args.directory:
+        if not os.path.isdir(args.directory):
+            print("Provided directory does not exist.")
+            sys.exit(1)
+        java_files = [os.path.join(args.directory, f) for f in os.listdir(args.directory) if f.endswith('.java')]
+        if not java_files:
+            print("No Java files found in the directory.")
+            sys.exit(1)
+    else:
+        print("No files or directory provided.")
         sys.exit(1)
 
-    java_files = sys.argv[1:]
-
-    print("concat working")
     with open("concatenated_output.txt", "w") as output_file:
         for java_file in java_files:
             with open(java_file, "r") as input_file:
                 # Print "// " before the filename
                 output_file.write(f"// {java_file}\n\n")
-                lines = input_file.readlines()
+                lines = input_file.read().splitlines()
                 processed_lines = process_lines(lines)
 
                 file_text = "".join(processed_lines)
@@ -32,6 +46,6 @@ def main():
 
                 output_file.write(file_text)
 
-
 if __name__ == "__main__":
     main()
+
